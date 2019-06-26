3627eb6b0b8ca5782f42da835da462e3b07a0bc4
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-26 10:14:30 -0700

    client/components/Login/

--- client/components/Login/Login.js
+++ client/components/Login/Login.js
@@ -4,37 +4,38 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 
 export default class Login extends React.Component {
-  constructor(){
-    super()
-    this.state={
+  constructor() {
+    super();
+    this.state = {
       user: null
-    }
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     let randomUser = 1;
     axios(`http://localhost:3000/api/james/?id=${randomUser}`)
-    .then(data=> this.setState({user: data.data}))
-    .catch(e=> console.log(e))
+      .then(data => this.setState({ user: data.data }))
+      .catch(e => console.log(e));
   }
 
-  render(){
+  render() {
     return (
       <View>
-        <View style={{backgroundColor: 'white'}}>
-        <Text>   </Text>
-        <Text>   </Text>
-        <Text>   </Text>
-        <Text>   </Text>
-        <Text>   </Text>
-        <Text>   </Text>
-        <Text>   </Text>
-        <Text>   </Text>
-        <Text>   </Text>
-        <Text>   </Text>
-        <Text>   </Text>
-        <Text>   </Text>
-        <Button onPress={() => this.props.handleLogin(this.state.user)}
+        <View style={{ backgroundColor: 'white' }}>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Button
+            onPress={() => this.props.handleLogin(this.state.user)}
             title="Login"
           />
         </View>
@@ -45,11 +46,11 @@ export default class Login extends React.Component {
 
 const styles = StyleSheet.create({
   container2: {
-    flex: 1,
-    paddingTop : Constants.statusBarHeight,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column'
+    // flex: 1,
+    // paddingTop: Constants.statusBarHeight,
+    backgroundColor: 'orange'
+    // alignItems: 'center',
+    // justifyContent: 'center'
+    // flexDirection: 'column'
   }
-})
+});
