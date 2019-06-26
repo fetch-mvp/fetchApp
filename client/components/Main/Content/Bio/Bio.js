8f70f83c7cc4058848bb7ca3ffd97a18acc20011
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-26 13:33:05 -0700

    Implemented Design and Edit Button

--- client/components/Main/Content/Bio/Bio.js
+++ client/components/Main/Content/Bio/Bio.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default class Bio extends React.Component {
-  constructor(props){
-    super(props)
-    this.state={}
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
   render() {
@@ -20,20 +20,21 @@ export default class Bio extends React.Component {
           />
         </View>
         <View>
-          <Text style={styles.textContainer}>
-            Name: {this.props.user.userName}
-          </Text>
-          <Text>
-            {' '}
-            My name is Bertha. Who be you? if you be cool we can rule...the
-            world that is because together we can move mountains...or just hike
-            them. I like hiking. You might think, but EmmyLou, you don't have
-            that much strength in your back legs to hike mountains" to which I
-            reply "Dang that's what rest is for! And physical therapy yo!We hike
-            one day. We rest two. You and me. Together. Forever. Hit me up. P.S.
-            that's me-the hot beagle Boston mix in the middle. With my homies.{' '}
-          </Text>
+          <Text style={styles.textContainer}>{this.props.user.userName}</Text>
+          <View style={styles.textbox}>
+            <Text style={styles.textContainer2}>
+              {' '}
+              My name is {this.props.user.userName}. Who be you? if you be cool
+              we can rule...the world that is because together we can move
+              mountains...or just hike them. I like hiking. You might think, but{' '}
+              {this.props.user.userName}, you don't have that much strength in
+              your back legs to hike mountains" to which I reply "Dang that's
+              what rest is for! And physical therapy yo!We hike one day. We rest
+              two. You and me. Together. Forever.
+            </Text>
+          </View>
         </View>
+        <Button title="Edit Profile" />
       </View>
     );
   }
@@ -42,8 +43,6 @@ export default class Bio extends React.Component {
 const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
-    // alignContent: 'stretch',
-    // marginTop: 20,
     paddingLeft: 1,
     paddingRight: 1
   },
@@ -55,18 +54,32 @@ const styles = StyleSheet.create({
   textContainer: {
     justifyContent: 'center',
     paddingTop: 370,
-    paddingBottom: 50
+    paddingBottom: 20,
+    fontSize: 30,
+    fontFamily: 'GillSans-SemiBoldItalic',
+    color: '#3EC1E1'
   },
   profile: {
-    color: 'blue',
+    color: '#3EC1E1',
     alignSelf: 'center',
-    paddingTop: 20,
-    fontSize: 50,
+    paddingTop: 30,
+    fontSize: 40,
     fontFamily: 'GillSans-SemiBoldItalic'
-  }
+  },
 
-  // textContainer2: {
-  //   justifyContent: 'center',
-  //   marginBottom: 400
-  // }
+  textContainer2: {
+    paddingLeft: 8,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+
+  textbox: {
+    borderRadius: 10,
+    backgroundColor: '#3EC1E1',
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 10,
+    paddingRight: 10
+  }
 });
+//3EC1E1 background color
