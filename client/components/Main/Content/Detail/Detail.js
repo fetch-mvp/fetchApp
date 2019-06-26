9ac1a6fee7aadef7b8bfc115208ec90158b1e8a3
Jongho Kim <april9288@gmail.com>
2019-06-26 13:05:32 -0700

    merging with calvin's

--- client/components/Main/Content/Detail/Detail.js
+++ client/components/Main/Content/Detail/Detail.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button, ImageBackground, Image } from "react-native";
+import { Pages } from "react-native-pages";
 
-export default class Detail extends React.Component {
-  constructor(){
-    super()
-    this.state={}
+export default class MoreInfo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
   }
 
-  render(){
+  render() {
+    const listImages = this.props.interestedDog.images.map((url, idx)=>
+    <ImageBackground
+    key = {idx}
+    source={{ uri: url }}
+    style={{ width: "100%", height: "100%", top: "0%" }}
+    imageStyle={{resizeMode: 'cover'}}
+  />
+    )
     return (
-      <View>
-        <Text>Detail</Text>
+      <View style={{ width: "100%", height: "100%"}}>
+        <View style={{position: 'absolute', left: "2%", top: "3%", zIndex: 1}}><Button onPress = {()=>{this.props.changeRoute('swipe')}} title="Back" /></View>
+        <View style={{ width: "100%", height: "60%"}}>
+        <Pages containerStyle={{}}>
+          {listImages}
+        </Pages>
+        </View>
+        <Text>{this.props.interestedDog.username}</Text>
+        <Image style={{width: 13, height: 13}} source={{uri: "https://img.icons8.com/ios/50/000000/male-filled.png"}}></Image>
+        <Text>{this.props.interestedDog.animalgender}</Text>
+        <Text>{"Location: " + this.props.interestedDog.location}</Text>
+        <Text>{"Description: " + this.props.interestedDog.description}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({});
