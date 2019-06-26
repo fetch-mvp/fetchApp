6b2f1edca4ecb9aabe960f72d2f11d09ff74b975
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-26 15:48:11 -0700

    update

--- client/components/Main/Content/Setting/Setting.js
+++ client/components/Main/Content/Setting/Setting.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ReactNativeSettingsPage, {
+  SectionRow,
+  NavigateRow,
+  CheckRow
+} from 'react-native-settings-page';
 
 export default class Setting extends React.Component {
-  constructor(){
-    super()
-    this.state={}
+  constructor() {
+    super();
+    this.state = {};
   }
 
-  render(){
-    return (
-      <View>
-        <Text>Setting</Text>
-      </View>
-    );
+  render() {
+    // return (
+    // )
+    <View>
+      <Text> Test</Text>
+    </View>;
   }
 }
