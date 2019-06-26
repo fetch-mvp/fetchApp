4f0b62e22e847e112e51a04e00df7bcd40f5ec89
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-26 16:39:25 -0700

    Added Route for EditProfile Component

--- client/components/Main/Content/Content.js
+++ client/components/Main/Content/Content.js
@@ -7,20 +7,41 @@ import Swipe from './Swipe/Swipe';
 import Detail from './Detail/Detail';
 import Match from './Match/Match';
 import Chat from './Chat/Chat';
+import EditProfile from './EditProfile/EditProfile.js';
 
-const Content = ({route, user, changeRoute, changeInterestedDog, interestedDog}) => {
-    if (route === 'bio') {
-      return <Bio user={user}/>
-    } else if (route === 'setting') {
-      return <Setting user={user}/>
-    } else if (route === 'swipe') {
-      return <Swipe changeInterestedDog = {changeInterestedDog} changeRoute = {changeRoute} user={user}/>
-    } else if (route === 'detail') {
-      return <Detail interestedDog = {interestedDog} changeRoute = {changeRoute} user={user}/>
-    } else if (route === 'match') {
-      return <Match user={user}/>
-    } else if (route === 'chat') {
-      return <Chat user={user}/>
-    }
-}
+const Content = ({
+  route,
+  user,
+  changeRoute,
+  changeInterestedDog,
+  interestedDog
+}) => {
+  if (route === 'bio') {
+    return <Bio user={user} />;
+  } else if (route === 'setting') {
+    return <Setting user={user} />;
+  } else if (route === 'swipe') {
+    return (
+      <Swipe
+        changeInterestedDog={changeInterestedDog}
+        changeRoute={changeRoute}
+        user={user}
+      />
+    );
+  } else if (route === 'detail') {
+    return (
+      <Detail
+        interestedDog={interestedDog}
+        changeRoute={changeRoute}
+        user={user}
+      />
+    );
+  } else if (route === 'match') {
+    return <Match user={user} />;
+  } else if (route === 'editprofile') {
+    return <EditProfile />;
+  } else {
+    return <Chat user={user} />;
+  }
+};
 export default Content;
