18818f22d999f9962ce01dbd161584332669c6d9
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-26 10:15:12 -0700

    Rendered Info

--- client/components/Main/Content/Bio/Bio.js
+++ client/components/Main/Content/Bio/Bio.js
@@ -13,39 +13,36 @@ export default class Bio extends React.Component {
     console.log(this.props.user);
     const { photo } = this.state;
     return (
-      <View style={styles.container}>
-        <Image
-          source={{ uri: `${this.props.user.images[0]}` }}
-          style={{ width: 300, height: 300 }}
-        />
-        <Text>Name: {this.props.user.userName}</Text>
-        <Text> Description</Text>
+      <View>
+        <View style={styles.imageContainer}>
+          <Image
+            source={{ uri: `${this.props.user.images[0]}` }}
+            style={{ width: 400, height: 350 }}
+          />
+        </View>
+        <View>
+          <Text style={styles.textContainer}>
+            Name: {this.props.user.userName}
+          </Text>
+          <Text> Description</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  imageContainer: {
     flex: 1,
-    // backgroundColor: 'blue',
-    alignContent: 'stretch',
-    // margin: 30,
+    // alignContent: 'stretch',
+    marginTop: 30,
     paddingLeft: 1,
     paddingRight: 1
   },
 
-  item: {
-    // flex: 1,
-    // overflow: 'hidden',
-    // alignItems: 'center',
-    // backgroundColor: 'orange',
-    // position: 'relative',
-    // margin: 10
-  },
-
-  image: {
-    // flex: 1
+  textContainer: {
+    justifyContent: 'center',
+    marginBottom: 50
   }
 });
 
