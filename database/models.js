7cf4537b26dc71e62fd1cfb72450dadd721dbf80
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-26 17:48:06 -0700

    Added to seed function

--- database/models.js
+++ database/models.js
@@ -1,43 +1,44 @@
 const mongoose = require('mongoose');
 
 const FetchSchema = new mongoose.Schema({
-	id : {
-		type: Number
-	},
-	userName : {
-		type: String
-	},
-	userPassword : {
-		type: String
-	},
-	userEmail : {
-		type: String
-	},
-	userLocation : {
-		type: String //zipcode
-	},
-	userSettingMiles : {
-		type: Number
-	},
-	description : {
-		type: String
-	},
-	preferences : {
-		type: [Number]
-	},
-	matches : {
-		type: [Number]
-	},
-	images : {
-		type: [String]
-	},
-	animalGender : {
-		type: Boolean // ture for female, false for male
-	},
-	animalBreed : {
-		type: String
-	}
-})
+  id: {
+    type: Number
+  },
+  userName: {
+    type: String
+  },
+  userPassword: {
+    type: String
+  },
+  userEmail: {
+    type: String
+  },
+  userLocation: {
+    type: String //zipcode
+  },
+  userSettingMiles: {
+    type: Number
+  },
+  description: {
+    type: String
+  },
+  preferences: {
+    type: [Number]
+  },
+  matches: {
+    type: [Number]
+  },
+  images: {
+    type: [String]
+  },
+  animalGender: {
+    type: Boolean // ture for female, false for male
+  },
+  animalSize: {
+    type: String // 'S', 'M', 'L'
+  },
+  swiped: []
+});
 
 const Fetch = mongoose.model('fetchs', FetchSchema);
 module.exports = Fetch;
--- database/seed.js
+++ database/seed.js
@@ -4,39 +4,48 @@ const db = require('./index.js');
 const Fetch = require('./models.js');
 
 const pics = [
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/beagle.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/chowcow.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/corgi.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/germanshepherd.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/goldenR.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/goldenretrieversheperd.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/pitbull.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/popeye.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/shiba-inu-corgi-mix.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/shiba.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/siberian-husky-price.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/chowcow.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/corgi.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/germanshepherd.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/goldenR.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/goldenretrieversheperd.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/pitbull.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/popeye.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/shiba-inu-corgi-mix.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/shiba.jpg",
-  "https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/siberian-husky-price.jpg"
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/beagle.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/chowcow.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/corgi.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/germanshepherd.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/goldenR.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/goldenretrieversheperd.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/pitbull.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/popeye.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/shiba-inu-corgi-mix.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/shiba.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/siberian-husky-price.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/chowcow.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/corgi.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/germanshepherd.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/goldenR.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/goldenretrieversheperd.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/pitbull.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/popeye.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/shiba-inu-corgi-mix.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/shiba.jpg',
+  'https://mvp-fetch-app.s3-us-west-1.amazonaws.com/Dog+Pictures/siberian-husky-price.jpg'
 ];
 
-let matchesGen = (i) => {
-  let samples = (i % 2 === 0) ? [1,3,5,7,9,11,13,15,17,19] : [2,4,6,8,10,12,14,16,18,20]
-  let a = Math.floor((Math.random() * 10))
-  let b = Math.floor((Math.random() * 10))
-  let c = Math.floor((Math.random() * 10))
-  let d = Math.floor((Math.random() * 10))
-  return [...new Set([samples[a],samples[b],samples[c],samples[d]])]
-}
+let matchesGen = i => {
+  let samples =
+    i % 2 === 0
+      ? [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+      : [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+  let a = Math.floor(Math.random() * 10);
+  let b = Math.floor(Math.random() * 10);
+  let c = Math.floor(Math.random() * 10);
+  let d = Math.floor(Math.random() * 10);
+  return [...new Set([samples[a], samples[b], samples[c], samples[d]])];
+};
 
-let container = []
+const animalSizeGenerator = () => {
+  let size = ['Small', 'Medium', 'Large'];
+  let random = Math.floor(Math.random() * 3);
+
+  return size[random];
+};
+let container = [];
 const dataGenerator = () => {
   for (let i = 0; i < 20; i++) {
     let oneDoc = {
@@ -45,20 +54,26 @@ const dataGenerator = () => {
       userPassword: faker.internet.password(),
       userEmail: faker.internet.email(),
       userLocation: faker.address.zipCode(),
-      userSettingMiles: (i % 2) ? 10 : 5,
-      preferences: (i % 2 === 0) ? [1,3,5,7,9,11,13,15,17,19] : [2,4,6,8,10,12,14,16,18,20],
+      userSettingMiles: i % 2 ? 10 : 5,
+      preferences:
+        i % 2 === 0
+          ? [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+          : [2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
       matches: matchesGen(i),
       images: [pics[i]],
-      animalGender: (i % 2 === 0) ? true : false
-    }
-    container.push(oneDoc)
+      animalGender: i % 2 === 0 ? true : false,
+      description: faker.lorem.sentence(),
+      animalSize: animalSizeGenerator()
+    };
+    container.push(oneDoc);
   }
-}
-dataGenerator()
+  return container;
+};
+console.log(dataGenerator());
 
 const insertSeedData = () => {
   Fetch.insertMany(container)
-  .then(()=> console.log("success"))
-  .catch(()=> console.log("error"))
-}
-insertSeedData()
+    .then(() => console.log('success'))
+    .catch(() => console.log('error'));
+};
+insertSeedData();
