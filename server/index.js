759e595be9bbaa11818da384034c063bb9fc68a1
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-27 15:15:21 -0700

    Implemented Route to Multer Storage

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ const db = require('../database/index');
 const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
-//Multer for Image Uploading
-const multer = require('multer');
 
 const calvinRouter = require('./calvin/router.js');
 const gabiRouter = require('./gabi/router.js');
@@ -15,26 +13,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-//Image Uploading Storage
-const Storage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, './images');
-  },
-  filename(req, file, callback) {
-    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
-  }
-});
-
-const upload = multer({ storage: Storage });
-
-app.post('/api/upload', upload.array('photo', 3), (req, res) => {
-  console.log('file', req.files);
-  console.log('body', req.body);
-  res.status(200).json({
-    message: 'success!'
-  });
-});
 
 //testing purpose
 app.get('/', (req, res) => {
--- server/wendy/router.js
+++ server/wendy/router.js
@@ -2,6 +2,30 @@ const routes = require('express').Router();
 const Fetch = require('../../database/models');
 const ObjectID = require('mongodb').ObjectID;
 
+//Multer for Image Uploading
+const multer = require('multer');
+
+//Image Uploading Storage
+const Storage = multer.diskStorage({
+  destination(req, file, callback) {
+    console.log('==>', file);
+    callback(null, './images');
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+  }
+});
+
+const upload = multer({ storage: Storage });
+
+routes.post('/upload', upload.array('photo', 1), (req, res) => {
+  console.log('file', req.files);
+  console.log('body', req.body);
+  res.status(200).json({
+    message: 'success!'
+  });
+});
+
 //Routers
 routes.get('/', (req, res) => {
   const { id } = req.query;
